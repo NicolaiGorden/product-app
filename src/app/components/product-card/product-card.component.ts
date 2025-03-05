f359d4caf89627c5e9c44f1a1a5f04473ffd4a4e
Nicolai <Nicolaigorden@gmail.com>
2025-03-05 02:31:13 -0500

    fixed routing, details page

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-product-card',
+  imports: [RouterModule],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
 
+export class ProductDetailComponent implements OnInit{
+  product: any;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'))
+    this.productService.getProductById(id).subscribe(data => {
+      this.product = data
+    })
+  }
 }
